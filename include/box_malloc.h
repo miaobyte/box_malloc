fc8ba80c704e4859cfb8d02f750bcf75d05e578b
peng.li24 <peng.li24@nio.com>
2025-09-02 09:06:22 +0000

    feat(box_malloc):init

---
+++ include/box_malloc.h
@@ -0,0 +1,10 @@
+#ifndef BOX_MALLOC_H
+#define BOX_MALLOC_H
+
+#include <stddef.h>
+
+int box_init(void *meta,size_t buddysize, void *boxstart, size_t box_size);
+void *box_alloc(void *meta, size_t size);
+void box_free(void *meta, void *ptr);
+
+#endif // BOX_MALLOC_H
